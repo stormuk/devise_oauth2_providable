3bcd3f09140b9cae1d0f0e34bc38e968a0055f62
Adam Pope <adam@storm-consultancy.com>
2012-07-16 14:20:10 +0100

    Allow preapproved apps for 2-legged oauth

--- app/controllers/devise/oauth2_providable/authorizations_controller.rb
+++ app/controllers/devise/oauth2_providable/authorizations_controller.rb
@@ -33,8 +33,8 @@ module Devise
         Rack::OAuth2::Server::Authorize.new do |req, res|
           @client = Client.find_by_identifier(req.client_id) || req.bad_request!
           res.redirect_uri = @redirect_uri = req.verify_redirect_uri!(@client.redirect_uri)
-          if allow_approval
-            if params[:approve].present?
+          if allow_approval || @client.preapproved
+            if params[:approve].present? || @client.preapproved
               case req.response_type
               when :code
                 authorization_code = current_user.authorization_codes.create!(:client => @client)
